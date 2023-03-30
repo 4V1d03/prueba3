001da7343ecb5866624df0a271183707202a37ca
Bryan Zavala <zavalarodriguez88@outlook.com>
2023-03-30 14:16:15 -0600

    prueba 2

--- ConexionSQL/Form1.cs
+++ ConexionSQL/Form1.cs
@@ -10,6 +10,8 @@ namespace ConexionSQL
         private void Form1_Load(object sender, EventArgs e)
         {
             //holaaaaaaa
+
+            //codigo para ver si funciona el repositorio
         }
 
 
